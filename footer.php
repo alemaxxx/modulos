2aa44bb4d7dbeb080e58263b3e6bb2347f10bd12
alemaxxx <davigruberjunior@gmail.com>
2021-06-22 15:49:51 -0300

    footer header images img

---
+++ footer.php
@@ -0,0 +1,11 @@
+<script src="js/jquery.min.js"></script>
+<script src="js/popper.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/main.js"></script>
+
+<script src="js/login.js" type="text/javascript"></script>
+<script src="js/sair.js" type="text/javascript"></script>
+
+<script src="js/redefinirsenha.js" type="text/javascript"></script>
+</body>
+</html>
